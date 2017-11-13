bcf912e1dfa9be0014c0c986edd01a7dc1e51ac7
Joel Brobecker <brobecker@adacore.com>
2017-11-13 11:28:05 -0800

    QGEN: Allow EPL copyright notices + notices can be within first 100 lines
    This patch adapts the 'qgen' repository's configuration to allow EPL
    copyright notices.
    
    It also extends the limit within which a copyright notice is expected
    to 100 lines, adding a comment explaining why that limit is introduced.
    
    For Q914-054.

---
+++ testsuite/tests/Q914-054__EPL_copyright_notices/test.py
@@ -0,0 +1,119 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_epl_single_year_mtl(self):
+        """Style check test against epl_single_year.mtl
+        """
+        self.set_year(2017)
+        p = self.run_style_checker('qgen', 'epl_single_year.mtl')
+	self.assertEqual(p.status, 0, p.image)
+        self.assertRunOutputEmpty(p)
+
+        # Try the same test, but with a dummy module name.
+        # It should fail, because we no longer include QGEN's specific
+        # config which allows EPL copyright notices.
+        p = self.run_style_checker('whatever', 'epl_single_year.mtl')
+	self.assertNotEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, """\
+epl_single_year.mtl:2: Copyright notice is not correctly formatted
+It must look like...
+
+    Copyright (C) 1992-2017, <copyright holder>
+
+... where <copyright holder> can be any of:
+    - `AdaCore'
+    - `Altran Praxis'
+    - `Altran UK Limited'
+    - `Free Software Foundation, Inc.'
+    - `AdaCore, Altran Praxis'
+    - `AdaCore and Altran UK Limited'
+    - `AdaCore, Altran UK Limited'
+    - `AdaCore and Altran UK'
+    - `AdaCore, Altran UK'
+""")
+
+    def test_epl_range_mtl(self):
+        """Style check test against epl_range.mtl
+        """
+        self.set_year(2017)
+        p = self.run_style_checker('qgen', 'epl_range.mtl')
+	self.assertEqual(p.status, 0, p.image)
+        self.assertRunOutputEmpty(p)
+
+        # Try the same test, but with a dummy module name.
+        # It should fail, because we no longer include QGEN's specific
+        # config which allows EPL copyright notices.
+        p = self.run_style_checker('whatever', 'epl_range.mtl')
+	self.assertNotEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, """\
+epl_range.mtl:2: Copyright notice is not correctly formatted
+It must look like...
+
+    Copyright (C) 1992-2017, <copyright holder>
+
+... where <copyright holder> can be any of:
+    - `AdaCore'
+    - `Altran Praxis'
+    - `Altran UK Limited'
+    - `Free Software Foundation, Inc.'
+    - `AdaCore, Altran Praxis'
+    - `AdaCore and Altran UK Limited'
+    - `AdaCore, Altran UK Limited'
+    - `AdaCore and Altran UK'
+    - `AdaCore, Altran UK'
+""")
+
+    def test_relpath_m(self):
+        """Style check test against relpath.m
+        """
+        self.set_year(2017)
+        p = self.run_style_checker('qgen', 'relpath.m')
+	self.assertEqual(p.status, 0, p.image)
+        self.assertRunOutputEmpty(p)
+
+        # Try the same test, but with a dummy module name.
+        # Since the file has a traditional copyright notice,
+        # the test is expected to pass also.
+        p = self.run_style_checker('whatever', 'relpath.m')
+	self.assertEqual(p.status, 0, p.image)
+        self.assertRunOutputEmpty(p)
+
+    def test_deep_notice(self):
+        """Style check test against deep_notice.m
+        """
+        self.set_year(2017)
+        p = self.run_style_checker('qgen', 'deep_notice.m')
+	self.assertEqual(p.status, 0, p.image)
+        self.assertRunOutputEmpty(p)
+
+        # Try the same test, but with a dummy module name.
+        # It should fail, because we no longer include QGEN's specific
+        # config which allows notices to be deeper than the default.
+        p = self.run_style_checker('whatever', 'deep_notice.m')
+	self.assertNotEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, """\
+deep_notice.m:100: Copyright notice must occur before line 24
+""")
+
+    def test_notice_too_deep_m(self):
+        """Style check test against notice_too_deep.m
+        """
+        self.set_year(2017)
+        p = self.run_style_checker('qgen', 'notice_too_deep.m')
+	self.assertNotEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, """\
+notice_too_deep.m:101: Copyright notice must occur before line 100
+""")
+
+        # Try the same test, but with a dummy module name.
+        # It should fail, because we no longer include QGEN's specific
+        # config which allows notices to be deeper than the default.
+        p = self.run_style_checker('whatever', 'notice_too_deep.m')
+	self.assertNotEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, """\
+notice_too_deep.m:101: Copyright notice must occur before line 24
+""")
+
+
+if __name__ == '__main__':
+    runtests()
